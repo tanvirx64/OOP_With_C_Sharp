918f46ba3eeecd0467d29139fe6ec0883e550d85
Tanvir Ahmed <tanvircsdev@gmail.com>
2024-01-21 21:09:18 +0600

    Use of Out Modifier and example of defensive coding

--- MethodOverriding/Program.cs
+++ MethodOverriding/Program.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace MethodOverriding
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
@@ -17,13 +18,40 @@ namespace MethodOverriding
             var calculator = new Calculator();
 
             //without params
-            calculator.Add(new int[] {1,2,3,4,5});
+            calculator.Add(new int[] { 1, 2, 3, 4, 5 });
 
 
             //with params
-            calculator.AddUsingParams(new int[] {1,2,4,5});
+            calculator.AddUsingParams(new int[] { 1, 2, 4, 5 });
             //or
-            calculator.AddUsingParams(1,2,4,5);
+            calculator.AddUsingParams(1, 2, 4, 5);
+
+            //Point
+            try
+            {
+                var point = new Point(0, 0);
+                point.Move(200, 300);
+                Console.WriteLine("{0},{1}", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("An Error Occured");
+            }
+
+
+            //use of out modifier
+            int number;
+            var result = int.TryParse(Console.ReadLine(), out number);
+            if (result)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed");
+            }
+            
         }
     }
 }
