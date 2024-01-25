0804cb2e42fd85e6285d3ee6a86771ac769c69a2
Tanvir Ahmed <tanvircsdev@gmail.com>
2024-01-25 20:55:40 +0600

    Use of Generics and Constraints

---
+++ Generics/DiscountCalculator.cs
@@ -0,0 +1,12 @@
+﻿namespace Generics
+{
+    public class DiscountCalculator<TProduct> where TProduct : Product
+    {
+        public float CalculateDiscount(TProduct product)
+        {
+            return product.Price;
+        }
+    }
+
+
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //GenericList<int> numbers = new GenericList<int>();
+            //GenericList<Book> books = new GenericList<Book>();
+            //books.Add(new Book { });
+            //numbers.Add(5);
+
+            Nullable<float> number = new Nullable<float>();
+            Console.WriteLine("Has value? : " + number.HasValue);
+            Console.WriteLine("Value : " + number.GetValueOrDefault());
+            
+        }
+    }
+}
