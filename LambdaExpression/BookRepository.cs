07356d09a9ce1aa4679ce108e389bd810229780e
Tanvir Ahmed <tanvircsdev@gmail.com>
2024-01-27 21:38:55 +0600

    Lambda Expression

---
+++ LambdaExpression/BookRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace LambdaExpression
+{
+    public class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>{
+                new Book() { Id = 1, Title="Introduction to C#", Price = 88.50M },
+                new Book() { Id = 2, Title="Introduction to JavaScript", Price = 110.50M },
+                new Book() { Id = 3, Title="Introduction to SQL", Price = 99.50M },
+            };
+        }
+    }
+}
---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LambdaExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  args => expression
+            // (arg1, arg2) => expression
+            // () => expression
+
+            //Func<int, int> square = s => s * s;
+            //const int factor = 5;
+            //Func<int, int> multiplier = number => number * factor;
+
+            //Console.WriteLine(square(5));
+            //Console.WriteLine(multiplier(5));
+
+            var books = new BookRepository().GetBooks();
+
+
+            var cheapBooks = books.FindAll(IsCheaperThan100USD);    //without lambda expression
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+
+            cheapBooks = books.FindAll(b => b.Price < 100);  //with lambda expression
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+
+        }
+
+        public static bool IsCheaperThan100USD(Book book)
+        {
+            return book.Price < 100;
+        }
+       
+    }
+}
