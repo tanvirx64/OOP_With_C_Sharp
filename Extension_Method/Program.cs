bd32bc0ef435694909ecba3116073c13bc257720
Tanvir Ahmed <tanvircsdev@gmail.com>
2024-01-29 23:49:37 +0600

    Extension Method and LINQ

---
+++ Extension_Method/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Extension_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Extension Method!");
+        }
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ");
+        }
+    }
+}
