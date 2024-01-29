6c1d08692c15277d4c829e5458b0de26abc53b80
Tanvir Ahmed <tanvircsdev@gmail.com>
2024-01-29 23:56:19 +0600

    LINQ updated

---
+++ LINQ/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace LINQ
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -18,9 +18,4 @@ namespace LINQ
             Console.WriteLine(lastBook.Name);
         }
     }
-    public class Book
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
 }
